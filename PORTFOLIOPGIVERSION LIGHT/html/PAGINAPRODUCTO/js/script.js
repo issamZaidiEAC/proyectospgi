fa42190b469d34e45037a13794736077f1716847
issamZaidiEAC <issam.zaidi@eaceuta.com>
2023-01-19 10:56:44 +0100

    PRACTICAS JUEVES 19/01
    Añado modal a pagina producto

--- PORTFOLIOPGIVERSION LIGHT/html/PAGINAPRODUCTO/js/script.js
+++ PORTFOLIOPGIVERSION LIGHT/html/PAGINAPRODUCTO/js/script.js
@@ -29,3 +29,29 @@ const dragStop = e => {
 gallery.addEventListener("mousedown", dragStart);
 gallery.addEventListener("mousemove", dragActive);
 gallery.addEventListener("mouseup", dragStop);
+
+const modalImgWrapper = document.getElementById("imgs");
+
+const toLeft = document.getElementById("izquierda");
+const toRight = document.getElementById("derecha");
+
+const imgIndex = 0;
+const imgs = ["../img/modal1.png", "../img/modal2.png", "../img/modal3.png"];
+
+toLeft.addEventListener("click", function(){
+    if (imgIndex = 0) {
+        imgIndex = 2;
+    } else {
+        imgIndex--;
+    }
+    modalImgWrapper.style.backgroundImage = imgs[index];
+})
+
+toLeft.addEventListener("click", function(){
+    if (imgIndex = 2) {
+        imgIndex = 0;
+    } else {
+        imgIndex++;
+    }
+    modalImgWrapper.style.backgroundImage = imgs[index];
+})
