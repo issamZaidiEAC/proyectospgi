4cb5c0461675be040ce9455b90dc1b71d5fb5dbf
issamZaidiEAC <issam.zaidi@eaceuta.com>
2023-01-19 12:56:24 +0100

    AÑADO INTERACCIONES AL MODAL
    PAGINA PRODUCTO

--- PORTFOLIOPGIVERSION LIGHT/html/PAGINAPRODUCTO/js/script.js
+++ PORTFOLIOPGIVERSION LIGHT/html/PAGINAPRODUCTO/js/script.js
@@ -35,23 +35,23 @@ const modalImgWrapper = document.getElementById("imgs");
 const toLeft = document.getElementById("izquierda");
 const toRight = document.getElementById("derecha");
 
-const imgIndex = 0;
-const imgs = ["../img/modal1.png", "../img/modal2.png", "../img/modal3.png"];
+let imgIndex = 0;
+const imgs = ["url(../img/modal1.png)", "url(../img/modal2.png)", "url(../img/modal3.png)"];
 
 toLeft.addEventListener("click", function(){
-    if (imgIndex = 0) {
+    if (imgIndex == 0) {
         imgIndex = 2;
     } else {
         imgIndex--;
     }
-    modalImgWrapper.style.backgroundImage = imgs[index];
+    modalImgWrapper.style.backgroundImage = imgs[imgIndex];
 })
 
-toLeft.addEventListener("click", function(){
-    if (imgIndex = 2) {
+toRight.addEventListener("click", function(){
+    if (imgIndex == 2) {
         imgIndex = 0;
     } else {
         imgIndex++;
     }
-    modalImgWrapper.style.backgroundImage = imgs[index];
+    modalImgWrapper.style.backgroundImage = imgs[imgIndex];
 })
